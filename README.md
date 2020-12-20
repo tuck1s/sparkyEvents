ef118eafeee27620fa7c78d3b8565ad52a06797e
tuck1s <steve.tuck@sparkpost.com>
2020-12-20 00:06:27 +0000

    -o option, stdout, show types/properties

--- README.md
+++ README.md
@@ -5,9 +5,12 @@
 # sparkyEvents (new)
 [![Build Status](https://travis-ci.com/tuck1s/sparkyEvents.svg?branch=master)](https://travis-ci.com/tuck1s/sparkyEvents)
 
-Simple command-line tool to retrieve SparkPost message events into a .CSV file.
+Command-line tool to retrieve SparkPost message events in `.CSV` format.
 
-> Dec 2020: Command-line options have changed! Now supports all event search parameters.
+> Dec 2020: Command-line options have changed!
+> - Now supports all API event search parameters
+> - Output to file or stdout
+> - Show possible event types / properties from your SparkPost account
 
 ## Easy installation
 
@@ -23,7 +26,7 @@ pipenv shell
 ```
 _Note: In the above commands, you may need to run `pip3` instead of `pip`._
 
-`./sparkyEvents.py -h` for usage info. Flags mirror the events search parameters in the [Events API documentation](https://developers.sparkpost.com/api/events/#events).
+`./sparkyEvents.py -h` for usage info. Flags are named as per the events search parameters in the [Events API documentation](https://developers.sparkpost.com/api/events/#events).
 
 ## Pre-requisites
 Set up the `sparkpost.ini` file as per the example.
@@ -32,8 +35,7 @@ Replace `<YOUR API KEY>` with your specific, private API key. The key needs the 
 
 `Host` is only needed for SparkPost EU, or for legacy Enterprise services with specific URLs. You can omit for [sparkpost.com](https://www.sparkpost.com/).
 
-`Properties` can be any of the [SparkPost Event Properties](https://www.sparkpost.com/docs/tech-resources/webhook-event-reference/). Definition can be split over lines
-using indentation, as per [Python .ini file structure](https://docs.python.org/3/library/configparser.html#supported-ini-file-structure).
+`Properties` can be any of the [SparkPost Event Properties](https://developers.sparkpost.com/api/events/#header-event-types). Definition can be split over lines using indentation, as per [Python .ini file structure](https://docs.python.org/3/library/configparser.html#supported-ini-file-structure).
 
 ## Examples
 
@@ -44,7 +46,7 @@ The syntax for specifying time ranges has changed, and takes times in UTC only, 
 The following example fetches events for the December 2020 Gmail outage.
 
 ```
-./sparkyEvents.py out6.csv --from 2020-12-14T22:00:00Z --to 2020-12-16T00:00:00Z --events bounce --bounce_classes 10 --reasons gsmtp
+./sparkyEvents.py -o out6.csv --from 2020-12-14T22:00:00Z --to 2020-12-16T00:00:00Z --events bounce --bounce_classes 10 --reasons gsmtp
 Writing to out6.csv
 from                     2020-12-14T22:00:00Z
 to                       2020-12-16T00:00:00Z
@@ -55,6 +57,33 @@ Properties:  ['timestamp', 'raw_rcpt_to', 'subaccount_id']
 Total events to fetch:  824
 Page      1: got    824 events in 1.290 seconds
 ```
+
+The progress messages are printed to `stderr` so you can pipe the actual utput somewhere, e.g. to [csvkit](https://csvkit.readthedocs.io/) tools.
+
+## Parameters that support keyword searching
+
+Some parameters support keyword searching. For keyword searches with whitespace, enclose your values in quotes, e.g.
+```
+./sparkyEvents.py out.csv --subjects "cool cats, hot dogs"
+```
+
+## Event Properties and Event Types
+The following special options do not actually fetch events; they show what's available from your SparkPost service.
+
+```
+./sparkyEvents.py --show_properties
+```
+```
+ab_test_id,ab_test_version,amp_enabled,bounce_class,campaign_id,click_tracking,customer_id,delv_method,device_token,display_name,dr_latency,error_code,event_description,event_id,fbtype,friendly_from,geo_ip,initial_pixel,injection_time,ip_address,ip_pool,mailfrom,message_id,msg_from,msg_size,num_retries,open_tracking,outbound_tls,queue_time,raw_rcpt_to,raw_reason,rcpt_hash,rcpt_meta,rcpt_subs,rcpt_tags,rcpt_to,rcpt_type,reason,recipient_domain,recv_method,remote_addr,report_by,report_to,routing_domain,scheduled_time,sending_domain,sending_ip,sms_coding,sms_dst,sms_dst_npi,sms_dst_ton,sms_remoteids,sms_segments,sms_src,sms_src_npi,sms_src_ton,sms_text,stat_state,stat_type,subaccount_id,subject,target_link_name,target_link_url,template_id,template_version,timestamp,transactional,transmission_id,type,user_agent
+```
+
+```
+./sparkyEvents.py --show_types
+```
+```
+amp_click,amp_initial_open,amp_open,bounce,click,delay,delivery,generation_failure,generation_rejection,initial_open,injection,link_unsubscribe,list_unsubscribe,open,out_of_band,policy_rejection,sms_status,spam_complaint
+```
+
 
 ## See Also
 [SparkPost Developer Hub](https://developers.sparkpost.com/)
--- sparkyEvents.py
+++ sparkyEvents.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 from __future__ import print_function
-import configparser, argparse, time, csv, requests
+import configparser, argparse, time, csv, requests, sys
 
 def kwdStr(s):
     """
@@ -27,17 +27,39 @@ def getMessageEvents(url, apiKey, params):
             elif (response.status_code == 429 and response.json()['errors'][0]['message'] == 'Too many requests') or \
                  (response.status_code == 502 and response.json()['errors'][0]['message'] == 'Could not proceed (502 error)'):
                 snooze = 30
-                print(response.json(),
+                eprint(response.json(),
                       '.. pausing {} seconds for rate-limiting'.format(snooze))
                 time.sleep(snooze)
                 continue                # try again
             else:
-                print('Error:', response.status_code, ':', response.text)
+                eprint('Error:', response.status_code, ':', response.text)
                 return None
 
     except ConnectionError as err:
-        print('error code', err)
+        eprint('error code', err)
         exit(1)
+
+
+def showDocs(url, types=False):
+    """
+    List available event properties and types
+    """
+    response = requests.request("GET", url, headers={'Accept': 'application/json'})
+    properties = set()
+    for i in response.json()['results']:
+        if types:
+            properties.add(i.get('type').get('sampleValue')) # accumulate all the event types
+        else:
+            for k, v in i.items():
+                properties.add(k) # accumulate all the properties reported across all event types
+    return sorted(properties)
+
+
+def eprint(*args, **kwargs):
+    """
+    Print to stderr - see https://stackoverflow.com/a/14981125/8545455
+    """
+    print(*args, file=sys.stderr, **kwargs)
 
 
 # -----------------------------------------------------------------------------------------
@@ -50,7 +72,7 @@ config.read_file(open(configFile))
 cfg = config['SparkPost']
 apiKey = cfg.get('Authorization', '')           # API key is mandatory
 if not apiKey:
-    print('Error: missing Authorization line in ' + configFile)
+    eprint('Error: missing Authorization line in ' + configFile)
     exit(1)
 baseUrl = 'https://' + cfg.get('Host', 'api.sparkpost.com')
 
@@ -62,30 +84,42 @@ fList = properties.replace('\r', '').replace('\n', '').split(',')  # Strip newli
 parser = argparse.ArgumentParser(
     description='Simple command-line tool to retrieve SparkPost message events into a .CSV file.',
     epilog='For keyword searches with whitespace, enclose your values in quotes, e.g. --reasons "gsmtp,ripe avocados". SparkPost API key, host, and properties are specified in {}.'.format(configFile))
-parser.add_argument('outfile', metavar='outfile.csv', type=argparse.FileType('w'), help='output filename (CSV format), must be writeable.')
+in1 = parser.add_argument_group()
+in1.add_argument('-o', '--outfile', metavar='outfile.csv', type=argparse.FileType('w'), default='-', help='output filename (CSV format), must be writeable. If omitted, prints to stdout.')
 # See: https://developers.sparkpost.com/api/events/#events. cursor and per_page are filled in by this tool
-parser.add_argument('--from', type=str, help='Datetime in format of YYYY-MM-DDTHH:MM:ssZ, inclusive. Value should be in UTC. If omitted, defaults to "24 hours ago"')
-parser.add_argument('--to', type=str, action='store',help='Datetime in format of YYYY-MM-DDTHH:MM:ssZ, exclusive. Value should be in UTC. If omitted, defaults to "1 minute ago"')
-parser.add_argument('--delimiter', type=str, help='string, if omitted default is ,')
-parser.add_argument('--event_ids', type=str, help='Comma delimited list of event IDs to search')
-parser.add_argument('--events', type=str, help='Comma delimited list of event types to search')
-parser.add_argument('--recipients', type=str, help='Comma delimited list of recipients to search')
-parser.add_argument('--recipient_domains', type=kwdStr, help='Comma delimited list of recipient domains to search. Supports keyword searching by domain segment')
-parser.add_argument('--from_addresses', type=str, help='Comma delimited list of friendly from addresses to search')
-parser.add_argument('--sending_domains', type=kwdStr, help='Comma delimited list of sending domains to search. Supports keyword searching by domain segment')
-parser.add_argument('--subjects', type=kwdStr, help='Comma delimited list of subject lines from the email header to search. Supports keyword searching')
-parser.add_argument('--bounce_classes', type=str, help='Comma delimited list of bounce classification codes to search. See Bounce Classification Codes')
-parser.add_argument('--reasons', type=kwdStr, help='Comma delimited list of Bounce/failure/rejection reason to search. Supports keyword searching')
-parser.add_argument('--campaigns', type=kwdStr, help='Comma delimited list of campaign IDs to search. Supports keyword searching')
-parser.add_argument('--templates', type=kwdStr, help='Comma delimited list of template IDs to search. Supports keyword searching')
-parser.add_argument('--sending_ips', type=str, help='Comma delimited list of sending IP addresses to search')
-parser.add_argument('--ip_pools', type=kwdStr, help='Comma delimited list of IP pool IDs to search. Supports keyword searching')
-parser.add_argument('--subaccounts', type=str, help='Comma delimited list of subaccount IDs to search')
-parser.add_argument('--messages', type=str, help='Comma delimited list of message IDs to search')
-parser.add_argument('--transmissions', type=str, help='Comma delimited list of transmission IDs to search')
-parser.add_argument('--ab_tests', type=kwdStr, help='Comma delimited list of A/B test IDs to search. Supports keyword searching')
-parser.add_argument('--ab_test_versions', type=str, help='Comma delimited list of version numbers of A/B tests to search. If provided, ab_tests parameter becomes required')
+in1.add_argument('--from', type=str, help='Datetime in format of YYYY-MM-DDTHH:MM:ssZ, inclusive. Value should be in UTC. If omitted, defaults to "24 hours ago"')
+in1.add_argument('--to', type=str, action='store',help='Datetime in format of YYYY-MM-DDTHH:MM:ssZ, exclusive. Value should be in UTC. If omitted, defaults to "1 minute ago"')
+in1.add_argument('--delimiter', type=str, help='string, if omitted default is ,')
+in1.add_argument('--event_ids', type=str, help='Comma delimited list of event IDs to search')
+in1.add_argument('--events', type=str, help='Comma delimited list of event types to search')
+in1.add_argument('--recipients', type=str, help='Comma delimited list of recipients to search')
+in1.add_argument('--recipient_domains', type=kwdStr, help='Comma delimited list of recipient domains to search. Supports keyword searching by domain segment')
+in1.add_argument('--from_addresses', type=str, help='Comma delimited list of friendly from addresses to search')
+in1.add_argument('--sending_domains', type=kwdStr, help='Comma delimited list of sending domains to search. Supports keyword searching by domain segment')
+in1.add_argument('--subjects', type=kwdStr, help='Comma delimited list of subject lines from the email header to search. Supports keyword searching')
+in1.add_argument('--bounce_classes', type=str, help='Comma delimited list of bounce classification codes to search. See Bounce Classification Codes')
+in1.add_argument('--reasons', type=kwdStr, help='Comma delimited list of Bounce/failure/rejection reason to search. Supports keyword searching')
+in1.add_argument('--campaigns', type=kwdStr, help='Comma delimited list of campaign IDs to search. Supports keyword searching')
+in1.add_argument('--templates', type=kwdStr, help='Comma delimited list of template IDs to search. Supports keyword searching')
+in1.add_argument('--sending_ips', type=str, help='Comma delimited list of sending IP addresses to search')
+in1.add_argument('--ip_pools', type=kwdStr, help='Comma delimited list of IP pool IDs to search. Supports keyword searching')
+in1.add_argument('--subaccounts', type=str, help='Comma delimited list of subaccount IDs to search')
+in1.add_argument('--messages', type=str, help='Comma delimited list of message IDs to search')
+in1.add_argument('--transmissions', type=str, help='Comma delimited list of transmission IDs to search')
+in1.add_argument('--ab_tests', type=kwdStr, help='Comma delimited list of A/B test IDs to search. Supports keyword searching')
+in1.add_argument('--ab_test_versions', type=str, help='Comma delimited list of version numbers of A/B tests to search. If provided, ab_tests parameter becomes required')
+in2 = parser.add_argument_group('Special options')
+in2.add_argument('--show_properties', action='store_true', help='List available event properties from your SparkPost service, then quit')
+in2.add_argument('--show_types', action='store_true', help='List available event types from your SparkPost service, then quit')
 p = vars(parser.parse_args()) # Use a dict, because 'from' is a Python reserved word and can't be accessed as an object member
+
+if p['show_properties']:
+    print(','.join(showDocs(baseUrl + '/api/v1/events/message/documentation')))
+    exit(0)
+
+if p['show_types']:
+    print(','.join(showDocs(baseUrl + '/api/v1/events/message/documentation', types=True)))
+    exit(0)
 
 # Also allow old behavior of getting events list from config file, if user did not specify in command-line args
 if not p['events']:
@@ -96,7 +130,7 @@ if not p['events']:
 # Write CSV file header
 fh = csv.DictWriter(p['outfile'], fieldnames=fList, restval='', extrasaction='ignore')
 fh.writeheader()
-print('Writing to {}'.format(p['outfile'].name))
+eprint('Writing to {}'.format(p['outfile'].name))
 del p['outfile'] # don't pass this as an API parameter
 
 # Build the API query parameters from command-line args that are set
@@ -106,10 +140,10 @@ qp = {
 }
 for k, v in p.items():
     if v:
-        print('{:24} {:24}'.format(k, str(v)))
+        eprint('{:24} {:24}'.format(k, str(v)))
         qp[k] = v
 
-print('Properties: ', fList)
+eprint('Properties: ', fList)
 morePages = True
 eventPage = 1
 url = baseUrl + '/api/v1/events/message'
@@ -126,8 +160,8 @@ while morePages:
     endT = time.time()
 
     if eventPage == 1:
-        print('Total events to fetch: ', res['total_count'])
-    print('Page {0:6d}: got {1:6d} events in {2:2.3f} seconds'.format(eventPage, len(res['results']), endT - startT))
+        eprint('Total events to fetch: ', res['total_count'])
+    eprint('Page {0:6d}: got {1:6d} events in {2:2.3f} seconds'.format(eventPage, len(res['results']), endT - startT))
 
     # Get the links from the response.  If there is a 'next' link, continue processing
     if 'links' in res and 'next' in res['links']:
